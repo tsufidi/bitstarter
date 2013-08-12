d1a5a3ac012e49cb65595c20dbf1447fc427750f
Omar Lodhi <tsufidi@gmail.com>
2013-08-12 03:47:38 +0000

    added response

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-    fs.readFileSync('index.html', 'utf8');
+    response.send(fs.readFileSync('index.html', 'utf8'));
 });
 
 var port = process.env.PORT || 5000;
