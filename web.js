e00354f0207d18c7fcf78ee1e214243a3ec8669c
Omar Lodhi <tsufidi@gmail.com>
2013-08-12 03:35:50 +0000

    reading index.html

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    fs.WriteFile('index.html', 'utf8');
 });
 
 var port = process.env.PORT || 5000;
